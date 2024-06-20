986649ba0c4f05143d04d942975424df6cd8ebdc
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-06-21 01:55:36 +0600

    profile button update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,12 @@ const Navbar = () => {
               className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
             >
               <li>
-                <a className="justify-between">Profile</a>
+                <Link
+                  href={`/student/${userAuth._id}`}
+                  className="justify-between"
+                >
+                  Profile
+                </Link>
               </li>
               {userAuth.role === "Admin" && (
                 <li>
