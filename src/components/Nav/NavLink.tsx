5d9502c148d729bc908002072cdf08874d372c6f
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2024-09-07 03:07:18 +0600

    silli optimization

--- src/components/Nav/NavLink.tsx
+++ src/components/Nav/NavLink.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 
 const NavLink = ({ to, label }: { to: string; label?: string }) => {
   const pathname = usePathname();
